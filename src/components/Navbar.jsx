2ed2affe582743f1ba7e07f5158b583f29b0f655
Nischal Saini <156769721+NischalSaini@users.noreply.github.com>
2025-01-27 01:53:35 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
     <div className='w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0'>
       <img src={assets.logo} alt="" className='w-20 sm:w-25' />
       {userData ?
-        <div className='w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group'>
+        <div className='w-8 h-8 flex justify-center items-center rounded-full bg-white text-black relative group'>
           {userData.name[0].toUpperCase()}
           <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10'>
             <ul className='list-none m-0 p-2 bg-gray-300 text-sm'>
